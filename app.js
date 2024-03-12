52f5fb7ffb46067d4769143090d5d1b9ba6e9f6e
omkaralmale <omkaralmale@gmail.com>
2024-03-12 23:46:21 +0530

    return response based on url

--- app.js
+++ app.js
@@ -4,9 +4,19 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+    if (req.url === '/home') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Welcome home');
+      } else if (req.url === '/about') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Welcome to About Us page');
+      } else if (req.url === '/node') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Welcome to my Node Js project');
+      }
 });
 
 server.listen(port, hostname, () => {
